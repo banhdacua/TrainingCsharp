b4cf885bce6fd30e883b66ed5b590e7204dd2ade
banhdacua <vuson2386@gmail.com>
2022-12-23 16:15:57 +0700

    up bai 2

--- BaiTapDanhGiaCSharp/Program.cs
+++ BaiTapDanhGiaCSharp/Program.cs
@@ -1,9 +1,21 @@
 ﻿using BaiTapDanhGiaCSharp;
 
+//Bai 1
 int[] nums = new int[] { 3,30,34,5,9 };
 Console.WriteLine($"Output: {bai1.FindMissingNumber(nums)}");
-    
-nums = new int[] { 3,30,34,5,9 };
+
+//Bai 2
+nums = new int[] { 2,0,2,1,1,0 };
+bai2.sort(nums);
+for (int i = 0; i < nums.Length; i++)
+{
+    Console.Write($"{nums[i]} ");
+}
+Console.WriteLine();
+
+
+//Bai 3
+nums = new int[] { 3,77,100,34,5,9 };
 
 Console.WriteLine($"Output: {bai3.SortAndMergeIntoMax(nums)}");
 
---
+++ BaiTapDanhGiaCSharp/bai2.cs
@@ -0,0 +1,21 @@
+﻿namespace BaiTapDanhGiaCSharp;
+
+public class bai2
+{
+    public static void sort(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] >= nums[j])
+                {
+                    var temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        
+    }
+}
